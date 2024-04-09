0a9a8d270d9c017a9f2074ccfe3426296f819de0
Emanuele Argonni <87581383+SuperMitic@users.noreply.github.com>
2024-04-09 20:14:39 +0200

    Pomodoro Timer

---
+++ script.js
@@ -0,0 +1,95 @@
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const resetButton = document.getElementById('reset-button');
+const workTimeInput = document.getElementById('work-time');
+const breakTimeInput = document.getElementById('break-time');
+const timerDisplay = document.getElementById('pomodoro-time');
+const work_or_break = document.getElementById('work-or-break');
+
+let interval;
+let isWorking = true;
+let totalSeconds = 5 * 60;
+let isTimerRunning = false;
+let totalTime = 0;
+let totalBreakTime = 0;
+
+function updateTimer() {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function startTimer() {
+
+    if (isTimerRunning) { 
+        return; 
+    }
+
+    isTimerRunning = true;
+    work_or_break.textContent = 'Working Time';
+    setTimer();
+
+    interval = setInterval(() => {
+      totalSeconds--;
+      updatePomodoro();
+      updateTimer();
+
+      if (totalSeconds === 0) {
+        if (isWorking) {
+          totalSeconds = breakTimeInput.value * 60;
+          work_or_break.textContent = 'Break Time';
+          isWorking = false;
+        } else {
+          totalSeconds = workTimeInput.value * 60;
+          work_or_break.textContent = 'Working Time';
+          isWorking = true;
+        }
+      }
+    }, 1000);
+}
+
+function setTimer() {
+    totalSeconds = workTimeInput.value * 60;
+    totalTime = totalSeconds;
+    totalBreakTime = breakTimeInput.value * 60;
+    updateTimer();
+}
+
+function stopTimer() {
+  if (isTimerRunning){
+    clearInterval(interval);
+    isTimerRunning = false;
+    work_or_break.textContent = 'Paused';
+  }
+}
+
+function resetTimer() {
+  stopTimer();
+  setTimer();
+  work_or_break.textContent = 'Pomodoro Timer';
+  document.getElementById('fill').style.height = `0%`;
+}
+
+function workTimeChange() {
+    if (isTimerRunning) {
+        return;
+    }
+    setTimer();
+}
+
+function updatePomodoro() {
+  let progress = 0;
+  if (isWorking) {
+    progress = (totalTime - totalSeconds) / totalTime * 100;
+  }
+  else {
+    progress = (totalSeconds) / totalBreakTime * 100;
+  }
+  document.getElementById('fill').style.height = `${progress}%`;
+}
+
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
+workTimeInput.addEventListener('change', workTimeChange);
